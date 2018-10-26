2eadd0edabcc2209b823de2535020fc0473c12c3
Daniel Sockwell <dsockwell@gmail.com>
2018-10-26 10:10:20 -0400

    Add db and related basic test (#9)

---
+++ server/db/db.test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+
+const { Pool } = require('pg');
+
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+  database: 'trailblazers-reviews',
+});
+
+it('should add and delete a test table without an error', done => {
+  pool.query(
+    `CREATE TABLE jestTest (
+      PersonID int,
+      LastName varchar(255),
+      FirstName varchar(255),
+      Address varchar(255),
+      City varchar(255) );`,
+    () => {
+      pool.query('DROP TABLE jestTest;', () => {
+        pool.end();
+        expect(true).to.equal(true);
+        done();
+      });
+    },
+  );
+});
