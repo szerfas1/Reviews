2da35705f590d454cc2219b64ad900a549bfb24a
Daniel Sockwell <dsockwell@gmail.com>
2018-10-26 23:29:56 -0400

    Set up npm scripts for seed file

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -11,51 +11,50 @@ const createQueryStr = `
     PRIMARY KEY (id)
   );`;
 
-const initReviewsTable = () => {
-  const setupTablePromise = new Promise(res => {
-    db.connect((connectionErr, client) => {
-      client
-        .query('BEGIN')
-        .then(() => {
-          client.query('DROP TABLE reviews');
-        })
-        .then(() => {
-          client.query(createQueryStr);
-        })
-        .then(() => {
-          for (let i = 1; i < 100; i++) {
-            if (Math.random() < 0.9 && i < 100) {
-              i--;
-            }
-            const insertQuery = {
-              text: `
+const insertPlaceholderData = (client, res) => {
+  for (let i = 1; i < 100; i++) {
+    if (Math.random() < 0.9 && i < 100) {
+      i--;
+    }
+    const insertQuery = {
+      text: `
   INSERT INTO reviews (
     review_id, rating, reviewer, title, body, recommend, helpful, unhelpful
   ) VALUES (
        $1,       $2,      $3,      $4,   $5,    $6,        $7,       $8
   )`,
-              values: [
-                i,
-                faker.random.number({ max: 3, min: 1 }) +
-                  faker.random.number({ max: 2, min: 0 }),
-                faker.internet.userName(),
-                faker.lorem.sentence(),
-                faker.lorem.paragraph(5),
-                faker.random.boolean(),
-                faker.random.number(47),
-                faker.random.number(22),
-              ],
-            };
-            client.query(insertQuery.text, insertQuery.values, () => {
-              if (i === 99) {
-                client.query('COMMIT', () => {
-                  client.end();
-                  res(true);
-                });
-              }
-            });
-          }
+      values: [
+        i,
+        faker.random.number({ max: 3, min: 1 }) +
+          faker.random.number({ max: 2, min: 0 }),
+        faker.internet.userName(),
+        faker.lorem.sentence(),
+        faker.lorem.paragraph(5),
+        faker.random.boolean(),
+        faker.random.number(47),
+        faker.random.number(22),
+      ],
+    };
+    client.query(insertQuery.text, insertQuery.values, () => {
+      if (i === 99) {
+        client.query('COMMIT', () => {
+          client.end();
+          res(true);
         });
+      }
+    });
+  }
+};
+
+const initReviewsTable = () => {
+  const setupTablePromise = new Promise(res => {
+    db.connect((connectionErr, client) => {
+      client.query('BEGIN').then(() => {
+        client
+          .query('DROP TABLE reviews')
+          .then(() => client.query(createQueryStr))
+          .then(() => insertPlaceholderData(client, res));
+      });
     });
   });
 
