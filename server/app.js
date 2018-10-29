fe2e523fb98ff0d47f4ddc2945cc5d00ab77bb05
Daniel Sockwell <dsockwell@gmail.com>
2018-10-29 18:50:25 -0400

    Add more tests for API `/reviews` endpoint

--- server/app.js
+++ server/app.js
@@ -5,23 +5,19 @@ const db = new Pool({ database: 'trailblazers_reviews' });
 
 const app = express();
 
-// Helpers
-const send404 = function(req, res) {
-  res.send('No data found', 404);
-};
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/reviews/:productId', (req, res) => {
   db.query(`SELECT * FROM reviews WHERE product_id=${req.params.productId}`)
     .then(queryResponse => {
-      db.end();
+      // db.end();
       if (queryResponse.rowCount === 0) {
-        send404(req, res);
+        res.status(404).send('No data found for that product');
+      } else {
+        res.send(queryResponse.rows);
       }
-      res.send(queryResponse.rows);
     })
-    .catch(() => send404(req, res));
+    .catch(() => res.status(400).send('Could not process productId'));
 });
 
-module.exports = app;
+module.exports = { app, db };
--- server/app.test.js
+++ server/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const { expect } = require('chai');
-const app = require('./app.js');
+const { app, db } = require('./app.js');
 
 describe('Server basics', () => {
   it('should return 200 for a basic request', done => {
@@ -18,7 +18,6 @@ describe('/reviews endpoint', () => {
     request(app)
       .get('/reviews/50')
       .then(response => {
-        console.log('DONE', done);
         expect(response.statusCode).to.equal(200);
         // This isn't ideal.  It is actually querying the DB, which makes it
         // slower and isn't necessary for testing the API.  There should be
@@ -26,4 +25,33 @@ describe('/reviews endpoint', () => {
         done();
       });
   });
+  it('should return a result for a valid request the second time', done => {
+    request(app)
+      .get('/reviews/50')
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        // This isn't ideal.  It is actually querying the DB, which makes it
+        // slower and isn't necessary for testing the API.  There should be
+        // a way to use a mock here, but I'm not sure what it is.
+        done();
+      });
+  });
+  it('should return 404 when the product is not in the db', done => {
+    request(app)
+      .get('/reviews/5000')
+      .then(response => {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+  });
+  it('should return 400 when the productId is malformed', done => {
+    request(app)
+      .get('/reviews/banana')
+      .then(response => {
+        expect(response.statusCode).to.equal(400);
+        done();
+      });
+  });
 });
+
+afterAll(() => db.end());
