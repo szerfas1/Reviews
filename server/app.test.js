77ea012cadfa7e858a67534f4163c3d3a0ae9817
Daniel Sockwell <dsockwell@gmail.com>
2018-10-29 15:28:16 -0400

    Add basic integration test

--- server/app.test.js
+++ server/app.test.js
@@ -12,3 +12,17 @@ describe('Server basics', () => {
       });
   });
 });
+
+describe('/reviews endpoint', () => {
+  it('should return a result for a valid request', done => {
+    request(app)
+      .get('/reviews/50')
+      .then(response => {
+        expect(response.statusCode).to.equal(200);
+        // This isn't ideal.  It is actually querying the DB, which makes it
+        // slower and isn't necessary for testing the API.  There should be
+        // a way to use a mock here, but I'm not sure what it is.
+        done();
+      });
+  });
+});
