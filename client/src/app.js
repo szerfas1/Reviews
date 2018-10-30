19230eda72a43547ff2f1f6c69fcce5c6bf04062
Daniel Sockwell <dsockwell@gmail.com>
2018-10-29 22:30:35 -0400

    Add basic react component
    Have not added tests yet; refactoring is needed

--- client/src/app.js
+++ client/src/app.js
@@ -11,10 +11,61 @@ const Main = styled.div`
   padding: 0 10px;
 `;
 
-const App = () => (
-  <Main>
-    <h1>Hello, world?</h1>
-  </Main>
-);
+const ReviewOverview = styled.div``;
+const Review = styled.div``;
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/reviews/47')
+      .then(response => response.json())
+      .then(json => {
+        this.setState(() => ({ reviews: json }));
+      });
+  }
+
+  render() {
+    const { reviews } = this.state;
+    return (
+      <Main>
+        <h1>Reviews</h1>
+        <ReviewOverview />
+        {reviews.map(review => {
+          const stars = [];
+          for (let i = 0; i < 5; i++) {
+            i < review.rating ? stars.push('&#9733;') : stars.push('&#9734;');
+          }
+          return (
+            <Review key={review.id}>
+              <h4>{review.reviewer}</h4>
+              <h3>{review.title}</h3>
+              <p>
+                {stars.map(star => (
+                  <span dangerouslySetInnerHTML={{ __html: star }} />
+                ))}
+              </p>
+              <p>{review.body}</p>
+              <p>
+                {review.recommend
+                  ? 'Yes, I recommend this product'
+                  : 'I do not recommend this product'}
+              </p>
+              <p>
+                Helpful? Yes: {review.helpful} No: {review.unhelpful}
+              </p>
+              <hr />
+            </Review>
+          );
+        })}
+      </Main>
+    );
+  }
+}
 export default App;
--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ const { Pool } = require('pg');
 const db = new Pool({ database: 'trailblazers_reviews' });
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
