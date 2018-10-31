1b1d97914d1b95f72ca19c923634871def980d1b
Daniel Sockwell <dsockwell@gmail.com>
2018-10-31 18:14:03 -0400

    Additional ci setup (#16)
    * Add husky and lint-staged
    
    * Add README badges
    
    * Revise for improved code quality
    
    * Fix test

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -10,32 +10,30 @@ const createQueryStr = `
     recommend boolean, helpful int, unhelpful int,
     PRIMARY KEY (id)
   );`;
+const insertQueryText = `
+  INSERT INTO reviews (
+    product_id, rating, reviewer, title, body, recommend, helpful, unhelpful
+  ) VALUES (
+       $1,       $2,      $3,      $4,   $5,    $6,        $7,       $8
+  )`;
 
 const insertPlaceholderData = (client, res) => {
   for (let i = 1; i < 100; i++) {
     if (Math.random() < 0.9 && i < 100) {
       i--;
     }
-    const insertQuery = {
-      text: `
-  INSERT INTO reviews (
-    product_id, rating, reviewer, title, body, recommend, helpful, unhelpful
-  ) VALUES (
-       $1,       $2,      $3,      $4,   $5,    $6,        $7,       $8
-  )`,
-      values: [
-        i,
-        faker.random.number({ max: 3, min: 1 }) +
-          faker.random.number({ max: 2, min: 0 }),
-        faker.internet.userName(),
-        faker.lorem.sentence(),
-        faker.lorem.paragraph(5),
-        faker.random.boolean(),
-        faker.random.number(47),
-        faker.random.number(22),
-      ],
-    };
-    client.query(insertQuery.text, insertQuery.values, () => {
+    const insertQueryValues = [
+      i,
+      faker.random.number({ max: 5, min: 1 }),
+      faker.internet.userName(),
+      faker.lorem.sentence(),
+      faker.lorem.paragraph(5),
+      faker.random.boolean(),
+      faker.random.number(47),
+      faker.random.number(22),
+    ];
+
+    client.query(insertQueryText, insertQueryValues, () => {
       if (i === 99) {
         client.query('COMMIT', () => {
           client.end();
