4d9db1c5336d8cea994f2dff796b425a1f330796
Daniel Sockwell <dsockwell@gmail.com>
2018-11-02 14:33:41 -0400

    Refactor to use props restructuring in argument
    This is a shift from
    
    ```
    props => {
      const { variableOfInterest }
    }
    
    ```
    to
    
    ```
    ({ variableOfInterest }) => {}
    ```
    
    Which means the same thing but is shorter/more readable.

--- client/src/components/ReviewHeader.js
+++ client/src/components/ReviewHeader.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ReviewHeader = props => {
-  const { reviewer, title, rating } = props;
+const Reviewer = styled.div`
+  font-weight: bold;
+  display: inline;
+`;
 
-  const Reviewer = styled.div`
-    font-weight: bold;
-    display: inline;
-  `;
+const ReviewTitle = styled.h3`
+  margin-top: 0;
+`;
 
-  const ReviewTitle = styled.h3`
-    margin-top: 0;
-  `;
+const Star = styled.span`
+  color: ${status => (status.filled ? '#bd5b0d' : 'grey')};
+  font-size: 1.3em;
+`;
 
-  const Star = styled.span`
-    color: ${status => (status.filled ? '#bd5b0d' : 'grey')};
-    font-size: 1.3em;
-  `;
-
+const ReviewHeader = ({ reviewer, title, rating }) => {
   const stars = [1, 2, 3, 4, 5].map(el => (
     <Star key={el} filled={el <= rating}>
       ★
