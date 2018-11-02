fcff6b2f5d2e675b5c7b175c4243ea5c73ec7018
Daniel Sockwell <dsockwell@gmail.com>
2018-11-02 14:25:08 -0400

    Refactor code to break up long functions
    CodeClimate detected three functions that exceeded 25 substantive lines
    of code.  In all three cases, this was because the functions included
    styled-components styles inside the function.  I've refactored all three
    to move the style above the function, which better separates the styling
    from the logic/structure of the component.

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,23 @@ import Review from './components/Review.js';
 import RatingSnapshot from './components/RatingSnapshot.js';
 import AverageRating from './components/AverageRating.js';
 
+const Main = styled.div`
+  margin: 40px auto;
+  max-width: 650px;
+  line-height: 1.6;
+  font-size: 18px;
+  color: #444;
+  padding: 0 10px;
+`;
+
+const ReviewHeader = styled.div`
+  height: 12em;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
 class App extends React.Component {
   constructor() {
     super();
@@ -32,23 +49,6 @@ class App extends React.Component {
 
   render() {
     const { reviews, ratings } = this.state;
-
-    const Main = styled.div`
-      margin: 40px auto;
-      max-width: 650px;
-      line-height: 1.6;
-      font-size: 18px;
-      color: #444;
-      padding: 0 10px;
-    `;
-
-    const ReviewHeader = styled.div`
-      height: 12em;
-    `;
-
-    const Title = styled.h1`
-      text-align: center;
-    `;
 
     return (
       <Main>
