4815318e7bf27814f184d270e4c38d8752803519
Daniel Sockwell <dsockwell@gmail.com>
2018-11-11 18:42:11 -0500

    Update db config to work with test env

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 const faker = require('faker');
 
-const db = new Pool({ database: 'ebdb' }); // change to ebdb
+/* global TEST_ENV */
+const db =
+  typeof TEST_ENV !== 'undefined'
+    ? new Pool({ database: 'trailblazers_reviews' })
+    : new Pool({ database: 'ebdb' });
 
 const createQueryStr = `
   CREATE TABLE reviews (
